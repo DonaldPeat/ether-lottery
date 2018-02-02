00907388db043941b9f615363c9f04ba27004b78
Don Peat <don@donaldpeat.com>
2018-02-01 19:33:46 -0800

    test: deploy contract, allow multiple accounts to enter, minimum ether requirement

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -19,3 +19,45 @@ beforeEach(async () => {
     .send({ from: accounts[0], gas : '1000000'});
     lottery.setProvider(provider);
 });
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  })
+
+  it('allows multiple accounts to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.002', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.002', 'ether')
+    });
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.002', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+
+    assert.equal(players.length, 3);
+  })
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try{
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 200 //arbitrary wei amount under the required amount
+      })
+    } catch (err) {
+
+    }
+  } )
+})
