00ac4b33baf24258e9aac2c69751791e3aa5b56e
Don Peat <don@donaldpeat.com>
2018-02-01 19:53:43 -0800

    test: ether sent to winner, player array reset

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -71,5 +71,24 @@ describe('Lottery Contract', () => {
     } catch (err){
       assert(err);
     }
+  });
+
+  it('sends money to winner and resets player array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('3', 'ether') //enough to notice change in account balance
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from:accounts[0]});
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert(difference > web3.utils.toWei('2.8', 'ether')); //account for gas spent
+    assert.equal(players.length, 0);
+
   })
 })
